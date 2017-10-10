43e48901877f0c9ba5d88e37f8e1d2bfac5e8971
connormcbreen <connormcbreen@gmail.com>
2017-10-09 19:48:02 -0700

    slightly changed for easy answers

--- src/Questions.java
+++ src/Questions.java
@@ -10,23 +10,23 @@
  */
 public class Questions {
     
-    String[] l1q1 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct"};
-    String[] l1q2 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct"};
-    String[] l1q3 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct"};
-    String[] l1q4 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct"};
-    String[] l1q5 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct"};
+    String[] l1q1 =   {"What is the derivative of X^2", "2X", "X/2", "X", "X^3", "2X"};
+    String[] l1q2 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Answer2"};
+    String[] l1q3 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Answer3"};
+    String[] l1q4 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Answer1"};
+    String[] l1q5 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Answer2"};
     String[][] lesson1 = {l1q1, l1q2, l1q3, l1q4, l1q5};
-    String[] l2q1 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct"};
-    String[] l2q2 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct"};
-    String[] l2q3 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct"};
-    String[] l2q4 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct"};
-    String[] l2q5 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct"};
+    String[] l2q1 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Answer2"};
+    String[] l2q2 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Answer3"};
+    String[] l2q3 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Answer1"};
+    String[] l2q4 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Answer2"};
+    String[] l2q5 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Answer3"};
     String[][] lesson2 = {l2q1, l2q2, l2q3, l2q4, l2q5};
-    String[] l3q1 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct"};
-    String[] l3q2 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct"};
-    String[] l3q3 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct"};
-    String[] l3q4 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct"};
-    String[] l3q5 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct"};
+    String[] l3q1 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Answer2"};
+    String[] l3q2 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Answer3"};
+    String[] l3q3 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Answer1"};
+    String[] l3q4 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Answer2"};
+    String[] l3q5 =   {"Question", "Answer1", "Answer2", "Answer3", "Answer4", "Answer4"};
     String[][] lesson3 = {l3q1, l3q2, l3q3, l3q4, l3q5};
     String[][][] lessons = {lesson1, lesson2, lesson3};
     
