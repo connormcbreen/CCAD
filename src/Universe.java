eb317d2360930bfb5dacd613f56caa7f04345186
d1david18 <31357615+d1david18@users.noreply.github.com>
2017-10-10 13:02:26 -0700

    Update Universe

--- src/Universe.java
+++ src/Universe.java
@@ -1,150 +1,214 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Class: Universe.java
+Creator: Daniel Davidson
+Date: 10/2/2017
+Description: This class contains a CardLayout that holds two main JPanels, the first is a login screen that is used to
+hold user information for later use, the second is the main lesson frame consisting of small JPanels with their own functionalities.
+Estimated hours: 12
 
-/**
- *
- * @author conno_000
- */
+*/
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URISyntaxException;
 import java.awt.event.WindowStateListener;
 import java.awt.event.WindowEvent;
-import javax.swing.JSlider;
+
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-public class Universe extends JFrame
-{
 
-    //Create Needed panels and labels and instantiate them
+public class Universe {
 
+    //Components for container JFrame
+    JFrame frame = new JFrame();
+    JPanel container = new JPanel();
+    JPanel panel1 = new JPanel();
+    JPanel panel2 = new JPanel();
+
+    //Components for login panel
+    private JLabel nameLbl = new JLabel("Name:");
+    private JLabel emailLbl = new JLabel("Email:");
+    private JLabel titleLbl = new JLabel("Please enter your information below to continue!");
+    private JTextField nameEntry = new JTextField(10);
+    private JTextField emailEntry = new JTextField(10);
+    private JPanel smallPanel = new JPanel();
+    private JPanel containerPanel = new JPanel();
+    private JButton continueBtn = new JButton();
+
+    //Components for main lesson Frame
     private JLabel labelFour = new JLabel("Daniel Davidson");
-    private JSlider sliderOne = new JSlider();
-
     Companion companionPanel = null;
     //Assessor assessorPanel = null;
     ConceptualQuestions conceptualQuestions = null;
     Tutor tutorPanel = null;
-
-
     private JPanel panelFour = new JPanel();
     private JPanel bottomPanel = new JPanel();
     private JPanel topPanel = new JPanel();
 
-    //Constructor
-    public Universe()
-    {
+    CardLayout layout = new CardLayout();
 
+    //Constructor
+    public Universe(){
         tutorPanel = new Tutor();
-        //assessorPanel = new Assessor();
         conceptualQuestions = new ConceptualQuestions(tutorPanel);
-        
         try {
             companionPanel = new Companion();
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
-
-        //Create action listener to listen for changes in the sliders state
-        sliderOne.addChangeListener(new ChangeListener() {
-
-            public void stateChanged(ChangeEvent e) {
-                if ( !( sliderOne.getValueIsAdjusting() ) ) {
-                    System.out.println(sliderOne.getValue());
-                    changeLabelVisibility();
-                    //Call change state on each panel class
-                    //tutorPanel.changeState(sliderOne.getValue());
-                    companionPanel.changeState(sliderOne.getValue());
-                    //assessorPanel.updateState(sliderOne.getValue());
-                }
-            }
-        });
-
-        //Style the JPanel with borders
+        //Style the individual JPanel with borders
         companionPanel.setBorder(BorderFactory.createLineBorder(Color.black));
         tutorPanel.setBorder(BorderFactory.createLineBorder(Color.black));
         conceptualQuestions.setBorder(BorderFactory.createLineBorder(Color.black));
         //assessorPanel.setBorder(BorderFactory.createLineBorder(Color.black));
         panelFour.setBorder(BorderFactory.createLineBorder(Color.black));
 
+        //Set a default opening size and layout
+        frame.setPreferredSize(new Dimension(700,700));
+        container.setLayout(layout);
 
-        //Set layout for main panel consisting of 2 smaller panels
-        setLayout(new BorderLayout());
+        //Add two login panel and main lesson panel to container panel
+        container.add(panel1,"1");
+        container.add(panel2,"2");
+
+        //Setting the default panel to be login panel upon first load
+        layout.show(container, "1");
+
+
+        frame.add(container);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+       setupLoginFrame();
+
+       setupMainFrame();
+    }
+
+
+
+    public static void main(String[] args) {
+        Universe system = new Universe();
+    }
+
+    private void setupLoginFrame(){
+        //Setting up login frame and components
+        panel1.setLayout(new BorderLayout());
+        Color greenColor = new Color(98, 136, 146);
+        Color offGreen = new Color(98,116,126);
+        containerPanel.setBackground(greenColor);
+
+        panel1.add(containerPanel, BorderLayout.CENTER);
+        containerPanel.setLayout(new GridLayout(3,1));
+        containerPanel.add(titleLbl);
+        containerPanel.add(smallPanel);
+
+        titleLbl.setBorder(new EmptyBorder(150,3,3,3));
+        titleLbl.setHorizontalAlignment(SwingConstants.CENTER);
+
+        smallPanel.setLayout(new GridBagLayout());
+        smallPanel.setBackground(offGreen);
+
+        GridBagConstraints constraint = new GridBagConstraints();
+        constraint.insets.bottom = 10;
+
+        constraint.gridx = 0;
+        constraint.gridy = 0;
+        constraint.anchor = GridBagConstraints.WEST;
+
+        smallPanel.add(nameLbl, constraint);
+        constraint.gridy++;
+        smallPanel.add(emailLbl, constraint);
+
+        constraint.gridx++;
+        constraint.gridy = 0;
+
+        smallPanel.add(nameEntry, constraint);
+        constraint.gridy++;
+        smallPanel.add(emailEntry, constraint);
+        constraint.gridy++;
+
+        smallPanel.add(continueBtn, constraint);
+        continueBtn.setText("Continue");
+
+        continueBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (nameEntry.getText().isEmpty() == true || emailEntry.getText().isEmpty() == true) {
+
+                    titleLbl.setText("Some fields appear to be missing");
+                    titleLbl.setForeground(Color.white);
+                }else {
+                    Profile userProfile = new Profile();
+                    userProfile.name = nameEntry.getText();
+                    userProfile.email = emailEntry.getText();
+
+                    System.out.println("\n User name is: " + userProfile.name);
+                    System.out.println("User email is: " + userProfile.email);
+                    panel1.setVisible(false);
+                    bottomPanel.setVisible(true);
+                    layout.show(container, "2");
+                }
+            }
+        });
+    }
+
+    private void setupMainFrame(){
+        //Setting up main lesson frame
+
+        panel2.setLayout(new BorderLayout());
         bottomPanel.setLayout(new FlowLayout());
         topPanel.setLayout(new GridLayout(2,2));
+
         //manage layouts
-        add(topPanel, BorderLayout.CENTER);
-        add(bottomPanel, BorderLayout.SOUTH);
+        panel2.add(topPanel, BorderLayout.CENTER);
+        panel2.add(bottomPanel, BorderLayout.SOUTH);
+        bottomPanel.setVisible(false);
         topPanel.setBorder(BorderFactory.createLineBorder(Color.black));
         bottomPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+
         //add all panels
         topPanel.add(companionPanel);
         topPanel.add(tutorPanel);
         topPanel.add(conceptualQuestions);
         //topPanel.add(assessorPanel);
         topPanel.add(panelFour);
-        bottomPanel.add(sliderOne);
-        addWindowStateListener(new maximizeButton());
+        frame.addWindowStateListener(new maximizeButton());
 
-        //Style the slider and set to zero to start
-        sliderOne.setSnapToTicks(true);
-        sliderOne.setValue(0);
-        sliderOne.setMaximum(4);
-        sliderOne.setMajorTickSpacing(1);
-        sliderOne.setPaintTicks(true);
-        sliderOne.setPaintLabels(true);
-        sliderOne.setLabelTable(sliderOne.createStandardLabels(1));
         panelFour.add(labelFour);
-        setSize(700,700);
-        sliderOne.setPreferredSize(new Dimension(getWidth() - getWidth() / 3 ,50));
-        setVisible(true);
-
-        //Method to set name name visibility
-    } public void changeLabelVisibility() {
-    if (sliderOne.getValue() != 0) {
-
-        labelFour.setVisible(false);
-    } else {
-
-        labelFour.setVisible(true);
-    }
-}
-    public static void main(String[] args)
-    {
-        Universe frame = new Universe();
-
+        panel2.setSize(700,700);
+        panel2.setVisible(false);
 
     }
-    
+
     boolean flag = false;
     private class maximizeButton implements WindowStateListener
-{
-    public void windowStateChanged(WindowEvent event)
     {
-        int originalState;
-        if(flag == false);
+        public void windowStateChanged(WindowEvent event)
         {
-        originalState = event.getNewState();
-        flag = true;
-        }
-        if(event.getNewState() == MAXIMIZED_BOTH)
-        {
-        System.out.println("max");
-        tutorPanel.setWindowSize("max");
-        tutorPanel.resize();
-        }
-        else if(event.getNewState() == originalState)
-        {
-            System.out.println("min");
-            tutorPanel.setWindowSize("min");
-            tutorPanel.resize();
+            int originalState;
+            if(flag == false);
+            {
+                originalState = event.getNewState();
+                flag = true;
+            }
+            if(event.getNewState() == frame.MAXIMIZED_BOTH)
+            {
+                System.out.println("max");
+                tutorPanel.setWindowSize("max");
+                tutorPanel.resize();
+            }
+            else if(event.getNewState() == originalState)
+            {
+                System.out.println("min");
+                tutorPanel.setWindowSize("min");
+                tutorPanel.resize();
+            }
         }
     }
 }
-    
-}
+
 
