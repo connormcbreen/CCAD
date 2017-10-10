ede6558e658704c63d7a29ff01bb6cc04e04572e
d1david18 <31357615+d1david18@users.noreply.github.com>
2017-10-10 15:25:21 -0700

    Update profile methods

--- src/Profile.java
+++ src/Profile.java
@@ -1,7 +1,30 @@
+/*
+Creator: Daniel Davidson
+Description: This class holds the users current information and progress as they complete lessons
+
+*/
+
 public class Profile {
 
     public String name = "";
     public String email = "";
+    public Double lesson1 = 0.0;
+    public Double lesson2 = 0.0;
+    public Double lesson3 = 0.0;
 
+    public String getEmail() {
+        return email;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
